107e54cafba15a523defe4dfe8018b14ac49756b
Maja Friedemann <44875405+MajaFriedemann@users.noreply.github.com>
2021-02-26 13:57:32 +0000

    fix

--- scripts/jpsych/jspsych-score-reveal.js
+++ scripts/jpsych/jspsych-score-reveal.js
@@ -54,7 +54,7 @@ jsPsych.plugins['jspsych-score-reveal'] = (function () {
         var bonusPayment  = round(overallScore * paymentPerCorrectDecision / 100, 2);
 
         dataObject["score"] = overallScore;
-        dataObject["bonus_payment"] = bonusPayment;
+        dataObject["bonus"] = bonusPayment;
 
         // // calculate current bonus
         // if (dots_jointOverallAccuracy >= trial.bonusThreshold) {
